f93adf6115251dedcf44006e384482704bdd49de
dsantor <dsantor@eryce.com>
2020-06-07 09:55:00 +0200

    init

---
+++ pushNotifications/src/test/java/com/example/daky/pushNotifications/PushNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daky.pushNotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
